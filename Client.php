df9375e0e738630bdfe6861b73c5e27f694d125e
Borales <bordun.alexandr@gmail.com>
2014-06-27 01:34:57 +0300

    Small update

--- Client.php
+++ Client.php
@@ -5,35 +5,60 @@
 
 namespace EuroMD\yandexMetrika;
 
-use yii\authclient\OAuth2;
+use yii\authclient\clients\YandexOAuth;
 use yii\base\Component;
+use yii\base\InvalidParamException;
 
 /**
  * @package EuroMD\yandexMetrika
  * @author Borales
+ *
+ * @property YandexOAuth $apiClient
  */
 class Client extends Component
 {
-	/** @var OAuth2 */
-	private $apiClient;
+	/** @var string */
+	public $clientID;
+	/** @var string */
+	public $clientSecret;
+
+	/** @var YandexOAuth */
+	private $_apiClient;
 	/** @var string Yandex API base URL */
-	private $apiBaseUrl = 'https://webmaster.yandex.ru/api/v2/';
+	private $_apiBaseUrl = 'https://webmaster.yandex.ru/api/v2/';
 
 	public function init()
 	{
 		parent::init();
-		$this->apiClient = new OAuth2();
-		$this->apiClient->apiBaseUrl = $this->apiBaseUrl;
+		if(!$this->clientID || !$this->clientSecret)
+			throw new InvalidParamException;
+	}
+
+	/**
+	 * API Client
+	 * @return YandexOAuth
+	 */
+	public function getApiClient()
+	{
+		if(!$this->_apiClient) {
+			$this->_apiClient = new YandexOAuth([
+				'clientId' => $this->clientID,
+				'clientSecret' => $this->clientSecret,
+				'apiBaseUrl' => $this->_apiBaseUrl
+			]);
+		}
+		return $this->_apiClient;
 	}
 
 	/**
 	 * Add Original Text
 	 * @param string $text
 	 * @param int $siteID
+	 * @return array
 	 */
 	public function addOriginalText($text, $siteID)
 	{
 		$text = urlencode($text);
-		$this->apiClient->api("hosts/$siteID/original-texts/", "POST", $text);
+		return $this->apiClient->api("hosts/$siteID/original-texts/", "POST", [$text]);
 	}
 }

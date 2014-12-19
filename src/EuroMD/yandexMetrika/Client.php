908a93ff6c8324e5894ae7cdbe033bbc85ab2e6c
Borales <bordun.alexandr@gmail.com>
2014-12-19 05:33:20 +0100

    Adding method EuroMD\yandexMetrika\OAuth2::rawApi() to pre-process simple string PSR formatting

--- src/EuroMD/yandexMetrika/Client.php
+++ src/EuroMD/yandexMetrika/Client.php
@@ -19,119 +19,124 @@ use yii\validators\StringValidator;
  */
 class Client extends Component
 {
-	const CODE_UNAUTHORIZED = 401;
+    const CODE_UNAUTHORIZED = 401;
 
-	/** @var int Minimum text lenght for Original Text */
-	const YANDEX_OT_MIN = 500;
-	/** @var int Maximum text lenght for Original Text */
-	const YANDEX_OT_MAX = 32000;
-	/** @var int Daily limit for Original Text */
-	const YANDEX_OT_DAILY_LIMIT = 100;
+    /** @var int Minimum text lenght for Original Text */
+    const YANDEX_OT_MIN = 500;
+    /** @var int Maximum text lenght for Original Text */
+    const YANDEX_OT_MAX = 32000;
+    /** @var int Daily limit for Original Text */
+    const YANDEX_OT_DAILY_LIMIT = 100;
 
-	/** @var string */
-	public $cacheComponent = 'cache';
-	/** @var string */
-	public $clientID;
-	/** @var string */
-	public $clientSecret;
-	/** @var OAuth2 */
-	private $_apiClient;
+    /** @var string */
+    public $cacheComponent = 'cache';
+    /** @var string */
+    public $clientID;
+    /** @var string */
+    public $clientSecret;
+    /** @var OAuth2 */
+    private $_apiClient;
 
-	public function init()
-	{
-		parent::init();
-		if (!$this->clientID || !$this->clientSecret)
-			throw new InvalidParamException;
-	}
+    public function init()
+    {
+        parent::init();
+        if (!$this->clientID || !$this->clientSecret) {
+            throw new InvalidParamException;
+        }
+    }
 
-	/**
-	 * API Client
-	 * @return OAuth2
-	 */
-	public function getApiClient()
-	{
-		if (!$this->_apiClient) {
-			$this->_apiClient = new OAuth2([
-				'clientId' => $this->clientID,
-				'clientSecret' => $this->clientSecret,
-				'cacheComponent' => $this->cacheComponent,
-			]);
-		}
-		return $this->_apiClient;
-	}
+    /**
+     * API Client
+     * @return OAuth2
+     */
+    public function getApiClient()
+    {
+        if (!$this->_apiClient) {
+            $this->_apiClient = new OAuth2([
+                'clientId' => $this->clientID,
+                'clientSecret' => $this->clientSecret,
+                'cacheComponent' => $this->cacheComponent,
+            ]);
+        }
+        return $this->_apiClient;
+    }
 
-	/**
-	 * Add Original Text
-	 * @param string $text
-	 * @param int $yandexSiteID
-	 * @throws InvalidParamException
-	 * @return string|bool
-	 */
-	public function addOriginalText($text, $yandexSiteID)
-	{
-		/** @var Cache $cache */
-		$cache = \Yii::$app->get($this->cacheComponent);
-		$cacheKey = "Yandex_OT_sent:" . $this->clientID . "_" . $this->clientSecret;
+    /**
+     * Add Original Text
+     * @param string $text
+     * @param int $yandexSiteID
+     * @throws InvalidParamException
+     * @return string|bool
+     */
+    public function addOriginalText($text, $yandexSiteID)
+    {
+        /** @var Cache $cache */
+        $cache = \Yii::$app->get($this->cacheComponent);
+        $cacheKey = "Yandex_OT_sent:" . $this->clientID . "_" . $this->clientSecret;
 
-		$sent = $cache->get($cacheKey);
-		if($sent === false) {
-			$sent = 0;
-		}
+        $sent = $cache->get($cacheKey);
+        if ($sent === false) {
+            $sent = 0;
+        }
 
-		if($sent >= self::YANDEX_OT_DAILY_LIMIT) {
-			throw new InvalidParamException("Daily limit exceeded!");
-		}
+        if ($sent >= self::YANDEX_OT_DAILY_LIMIT) {
+            throw new InvalidParamException("Daily limit exceeded!");
+        }
 
-		$validator = new StringValidator([
-			'min' => self::YANDEX_OT_MIN,
-			'max' => self::YANDEX_OT_MAX,
-			'enableClientValidation' => false,
-		]);
+        $validator = new StringValidator([
+            'min' => self::YANDEX_OT_MIN,
+            'max' => self::YANDEX_OT_MAX,
+            'enableClientValidation' => false,
+        ]);
 
-		if (!$validator->validate($text, $error)) {
-			throw new InvalidParamException($error);
-		}
+        if (!$validator->validate($text, $error)) {
+            throw new InvalidParamException($error);
+        }
 
-		$text = urlencode(htmlspecialchars($text));
-		$text = "<original-text><content>{$text}</content></original-text>";
+        $text = urlencode(htmlspecialchars($text));
+        $text = "<original-text><content>{$text}</content></original-text>";
 
-		$response = $this->apiClient->api("hosts/$yandexSiteID/original-texts/", "POST", $text);
+        $response = $this->apiClient->rawApi("hosts/$yandexSiteID/original-texts/", "POST", $text);
 
-		$success = ArrayHelper::keyExists('id', $response) && ArrayHelper::keyExists('link', $response);
-		if($success) {
-			$sent++;
-			$cache->set($cacheKey, $sent, 60 * 60 * 24);
-		}
-		return $success;
-	}
+        $success = ArrayHelper::keyExists('id', $response) && ArrayHelper::keyExists('link', $response);
+        if ($success) {
+            $sent++;
+            $cache->set($cacheKey, $sent, 60 * 60 * 24);
+        }
+        return $success;
+    }
 
-	/**
-	 * @param string $code
-	 */
-	public function setCode($code)
-	{
-		$this->apiClient->fetchAccessToken($code);
-	}
+    /**
+     * @param string $code
+     */
+    public function setCode($code)
+    {
+        $this->apiClient->fetchAccessToken($code);
+    }
 
-	/**
-	 * Auth header
-	 * @return array
-	 * @throws InvalidParamException
-	 */
-	protected function getAuthHeader()
-	{
-		if ($this->apiClient->accessToken && $this->apiClient->accessToken->isValid) {
-			return 'Authorization: OAuth ' . $this->apiClient->accessToken->token;
-		}
-		throw new InvalidParamException('NOT VALID ACCESS TOKEN', self::CODE_UNAUTHORIZED);
-	}
+    /**
+     * Auth header
+     * @return array
+     * @throws InvalidParamException
+     */
+    protected function getAuthHeader()
+    {
+        if ($this->apiClient->accessToken && $this->apiClient->accessToken->isValid) {
+            return 'Authorization: OAuth ' . $this->apiClient->accessToken->token;
+        }
+        throw new InvalidParamException('NOT VALID ACCESS TOKEN', self::CODE_UNAUTHORIZED);
+    }
 
-	/**
-	 * Authorization url
-	 * @return string
-	 */
-	public function getAuthorizeURL()
-	{
-		return $this->apiClient->buildAuthUrl(['display' => 'iframe', 'redirect_uri' => null, 'xoauth_displayname' => null]);
-	}
+    /**
+     * Authorization url
+     * @return string
+     */
+    public function getAuthorizeURL()
+    {
+        return $this->apiClient->buildAuthUrl([
+            'display' => 'iframe',
+            'redirect_uri' => null,
+            'xoauth_displayname' => null
+        ]);
+    }
 }
--- src/EuroMD/yandexMetrika/OAuth2.php
+++ src/EuroMD/yandexMetrika/OAuth2.php
@@ -16,112 +16,114 @@ use yii\caching\Cache;
  */
 class OAuth2 extends YandexOAuth
 {
-	/** @var string */
-	public $cacheComponent = 'cache';
-	/** @var string Yandex API base URL */
-	public $apiBaseUrl = 'https://webmaster.yandex.ru/api/v2';
+    /** @var string */
+    public $cacheComponent = 'cache';
+    /** @var string Yandex API base URL */
+    public $apiBaseUrl = 'https://webmaster.yandex.ru/api/v2';
 
-	/**
-	 * @param string $apiSubUrl
-	 * @param string $method
-	 * @param array|string $params
-	 * @param array $headers
-	 * @return array
-	 */
-	public function api($apiSubUrl, $method = 'GET', $params = [], array $headers = [])
-	{
-		if(!is_array($params)) {
-			$headers[] = 'Content-Length: ' . mb_strlen($params);
-			$this->setCurlOptions([CURLOPT_POSTFIELDS => $params]);
-			$params = [];
-		}
-		return parent::api($apiSubUrl, $method, $params, $headers);
-	}
+    /**
+     * Pre-processing $params in case if string was given.
+     * @param string $apiSubUrl
+     * @param string $method
+     * @param array|string $params
+     * @param array $headers
+     * @return array
+     */
+    public function rawApi($apiSubUrl, $method = 'GET', $params = [], array $headers = [])
+    {
+        if (is_string($params)) {
+            $headers[] = 'Content-Length: ' . mb_strlen($params);
+            $this->setCurlOptions([CURLOPT_POSTFIELDS => $params]);
+            $params = [];
+        }
+        return $this->api($apiSubUrl, $method, $params, $headers);
+    }
 
-	/**
-	 * @param string $method
-	 * @param string $url
-	 * @param array $params
-	 * @return array
-	 */
-	protected function composeRequestCurlOptions($method, $url, array $params)
-	{
-		$options = parent::composeRequestCurlOptions($method, $url, $params);
-		if($method == 'POST' && !$params) {
-			unset($options[CURLOPT_POSTFIELDS], $options[CURLOPT_HTTPHEADER]);
-		}
-		return $options;
-	}
+    /**
+     * @param string $method
+     * @param string $url
+     * @param array $params
+     * @return array
+     */
+    protected function composeRequestCurlOptions($method, $url, array $params)
+    {
+        $options = parent::composeRequestCurlOptions($method, $url, $params);
+        if ($method == 'POST' && !$params) {
+            unset($options[CURLOPT_POSTFIELDS], $options[CURLOPT_HTTPHEADER]);
+        }
+        return $options;
+    }
 
-	/**
-	 * @param \yii\authclient\OAuthToken $accessToken
-	 * @param string $url
-	 * @param string $method
-	 * @param array $params
-	 * @param array $headers
-	 * @throws InvalidParamException
-	 * @return array
-	 */
-	protected function apiInternal($accessToken, $url, $method, array $params, array $headers)
-	{
-		$headers[] = 'Authorization: OAuth ' . $accessToken->token;
-		try {
-			return $this->sendRequest($method, $url, $params, $headers);
-		} catch (InvalidResponseException $e) {
-			if($e->responseHeaders['http_code'] == 201) {
-				return $this->processResponse($e->responseBody, $this->determineContentTypeByHeaders($e->responseHeaders));
-			}
-			throw new InvalidParamException('Request failed with code: ' . $e->responseHeaders['http_code'] . ', message: ' . $e->responseBody);
-		}
-	}
+    /**
+     * @param \yii\authclient\OAuthToken $accessToken
+     * @param string $url
+     * @param string $method
+     * @param array $params
+     * @param array $headers
+     * @throws InvalidParamException
+     * @return array
+     */
+    protected function apiInternal($accessToken, $url, $method, array $params, array $headers)
+    {
+        $headers[] = 'Authorization: OAuth ' . $accessToken->token;
+        try {
+            return $this->sendRequest($method, $url, $params, $headers);
+        } catch (InvalidResponseException $e) {
+            if ($e->responseHeaders['http_code'] == 201) {
+                return $this->processResponse($e->responseBody,
+                    $this->determineContentTypeByHeaders($e->responseHeaders));
+            }
+            throw new InvalidParamException('Request failed with code: ' . $e->responseHeaders['http_code'] . ', message: ' . $e->responseBody);
+        }
+    }
 
-	/**
-	 * @param string $key
-	 * @param mixed $value
-	 * @return $this|static
-	 */
-	protected function setState($key, $value)
-	{
-		$key = $this->getStateKeyPrefix() . $key;
-		$duration = $this->getCacheDuration();
-		$this->getCache()->set($key, $value, $duration);
-		return $this;
-	}
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @return $this|static
+     */
+    protected function setState($key, $value)
+    {
+        $key = $this->getStateKeyPrefix() . $key;
+        $duration = $this->getCacheDuration();
+        $this->getCache()->set($key, $value, $duration);
+        return $this;
+    }
 
-	/**
-	 * @param string $key
-	 * @return mixed
-	 */
-	protected function getState($key)
-	{
-		$key = $this->getStateKeyPrefix() . $key;
-		return $this->getCache()->get($key);
-	}
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    protected function getState($key)
+    {
+        $key = $this->getStateKeyPrefix() . $key;
+        return $this->getCache()->get($key);
+    }
 
-	/**
-	 * @param string $key
-	 * @return bool
-	 */
-	protected function removeState($key)
-	{
-		$key = $this->getStateKeyPrefix() . $key;
-		$this->getCache()->delete($key);
-		return true;
-	}
+    /**
+     * @param string $key
+     * @return bool
+     */
+    protected function removeState($key)
+    {
+        $key = $this->getStateKeyPrefix() . $key;
+        $this->getCache()->delete($key);
+        return true;
+    }
 
-	/**
-	 * @return Cache
-	 */
-	private function getCache()
-	{
-		return \Yii::$app->get($this->cacheComponent);
-	}
+    /**
+     * @return Cache
+     */
+    private function getCache()
+    {
+        return \Yii::$app->get($this->cacheComponent);
+    }
 
-	/**
-	 * @return int
-	 */
-	private function getCacheDuration()
-	{
-		return 24 * 60 * 60 * 180; // 180 days
-	}
+    /**
+     * @return int
+     */
+    private function getCacheDuration()
+    {
+        return 24 * 60 * 60 * 180; // 180 days
+    }
 }
